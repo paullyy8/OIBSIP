ddb047bb6bb77d35ae9dfd3165e2dabd5345aa91
paullyy8 <>
2024-10-29 21:54:22 +0530

    enhanced the prompts for user

--- PASSKEY/main.py
+++ PASSKEY/main.py
@@ -1,9 +1,9 @@
 import random
 
 # Prompt the user to enter the length
-length = int(input("Enter the length of the password to generate : "))
+length = int(input("\n" "Enter the desired length for your password: "))
 
-# We manually define the possible characters the password can contain
+# Define the possible characters the password can contain
 upper_case_letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 lower_case_letters = "abcdefghijklmnopqrstuvwxyz"
 digits = "0123456789"
@@ -15,9 +15,12 @@ char = upper_case_letters + lower_case_letters + digits + special_characters
 # Generate a random password by picking random characters from all_characters
 password = ""
 
-# Loop 'length' times to generate each character of the password
+# Loop to generate each character of the password
 for i in range(length):
     password += random.choice(char)
 
-# Output the randomly generated password
-print("Your password is:", password)
+# Output the randomly generated password with a box around it
+print("\n" + "-" * 50)
+print(f"Your generated password is 🔑 : {password}")
+print("-" * 50)
+print("\nMake sure to save it in a secure place! 💾")
