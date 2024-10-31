f4f58db43038c06c9c4ba76a5ec9827ea3bbfc41
paullyy8 <>
2024-11-01 01:17:02 +0530

    correct formula to calculate the BMI

--- FitCalc/script.py
+++ FitCalc/script.py
@@ -1,11 +1,11 @@
 print("Welcome to FitCalc -- BMI Calculator")
 
-#Prompt for user to fetch the weight and height 
+#Prompt for user to fetch the weight and height
 weight = float(input("Enter your weight (in kilograms): "))
 height = float(input("Enter your height (in meters): "))
 
 #formula to calculate the BMI
-BMI = weight / (height * height)
+BMI = weight / (height / 100) ** 2
 
 #making the BMI as round figure
 BMI_rounded = round(BMI, 2)
@@ -14,7 +14,7 @@ BMI_rounded = round(BMI, 2)
 if BMI < 18.5:
     classification = "underweight"
 elif 18.5 <= BMI < 24.9:
-    classification = "normal weight"
+    classification = "healthy"
 elif 25 <= BMI < 29.9:
     classification = "overweight"
 else:
